a9c36734aade2f67817fee763cfe609c6c5ee41d
Will19 <wiliamgomesc19@gmail.com>
2015-03-18 22:41:11 -0300

    Update 01q11.c

--- 01q11.c
+++ 01q11.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
 
+float fahrcel(float f){
+	return ((5*(f+32))/9);
+}
+
 int main(){
 	float t;
 	printf("Digite a temperatura em fahrenheit\n");
 	scanf("%f", &t);
-	printf("%f", ((5*(t+32))/9));
+	printf("%f", fahrcel(t));
 	return 0;
 }
