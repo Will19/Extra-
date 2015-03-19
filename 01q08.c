385ce2b378f805a6204a4fe0f4dc4161882516b0
Will19 <wiliamgomesc19@gmail.com>
2015-03-18 22:31:55 -0300

    Update and rename 01q8.c to 01q08.c

---
+++ 01q08.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+float arearaio (float i){
+	return (3,141592)*(i*i);
+}
+
+int main(){
+	float r;
+	scanf("%f", &r);
+	printf("%f\n", arearaio(r));
+	return 0;
+}
--- 01q8.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main(){
-	float r;
-	scanf("%f", &r);
-	printf("%f\n", (3,141592)*(r*r));
-	return 0;
-}
-
