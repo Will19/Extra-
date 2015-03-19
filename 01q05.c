e51dec915ce2ffdc6b83a20afe972ba8d08f0f04
Will19 <wiliamgomesc19@gmail.com>
2015-03-18 22:22:10 -0300

    Update and rename 01q5.c to 01q05.c

--- 01q05.c
+++ 01q05.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
 
+int soma(int i, int j){
+	return i+j;
+}
+
 int main(){
 	int x, y;
 	printf("Digite dois numeros\n");
 	scanf("%d %d", &x, &y);
-	printf("%d\n", x+y);
+	printf("%d\n", soma(x, y));
 	return 0;
 }
