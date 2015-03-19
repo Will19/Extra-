64eb262890ac5638d8b8d3759abba2974b3d112f
Will19 <wiliamgomesc19@gmail.com>
2015-03-18 22:16:30 -0300

    Update 01q3.c
    Questão 3 - pedir um número na tela e informá-lo.

--- 01q3.c
+++ 01q3.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
 
-int main(){
+void informanum(){
 	int x;
 	printf("Digite um numero\n");
 	scanf("%d", &x);
-	printf("O numero informado foi %d\n", x);
+	printf("O numero informado foi %d\n", x);	
+}
+
+int main(){
+	informanum();
 	return 0;
 }
