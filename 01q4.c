894fad959da0e5e6cf49f52d471278a8594adb8f
Will19 <wiliamgomesc19@gmail.com>
2015-03-18 22:19:52 -0300

    Update 01q4.c
    Questão 4 - soma de 4 números

--- 01q4.c
+++ 01q4.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 
+int soma(int a, int b, int c, int d){
+	return a+b+c+d;
+}
+
 int main(){
 	int x, y, z, w;
 	scanf("%d %d %d %d", &x, &y, &z, &w);
-	printf("%d\n", x+y+z+w);
+	printf("%d\n", soma(x, y, z, w));
 	return 0;
 }
